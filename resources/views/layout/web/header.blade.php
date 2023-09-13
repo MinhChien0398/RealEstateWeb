623f080244191dad42a2a80b9002b5632cb61aae
MinhTan <21130171@st.hcmuaf.edu.vn>
2023-09-14 02:44:20 +0700

    update MDB4, Font-Awesome, Footer, create tree work flow. Again because missing config

--- resources/views/layout/web/header.blade.php
+++ resources/views/layout/web/header.blade.php
@@ -1,1 +1,6 @@
-<?php
+<link href="{{asset('/lib/MDB_4_pro/css/bootstrap.min.css')}}" rel="stylesheet">
+<link href="{{asset('/lib/MDB_4_pro/css/mdb.lite.min.css')}}" rel="stylesheet">
+<link href="{{asset('/lib/MDB_4_pro/css/mdb.min.css')}}" rel="stylesheet">
+<link href="{{asset('/lib/MDB_4_pro/css/style.css')}}" rel="stylesheet">
+{{--<link href="{{asset('/lib/fontawesome-free-6.4.2-web/css/fontawesome.min.css')}}" rel="stylesheet">--}}
+<link href="{{asset('/lib/fontawesome-free-6.4.2-web/css/all.min.css')}}" rel="stylesheet">
