44c86e22ed63122a45f3af6d86b06528c88813a1
Xuan Chien Vu <137017493+xuanchiengigido@users.noreply.github.com>
2023-09-20 14:37:40 +0700

    Update nav bar
    * push template folder
    
    * update folder template
    
    * update main views
    
    * update navBar
    
    ---------
    
    Co-authored-by: Minh Tấn <136873753+tandang2003@users.noreply.github.com>
    Co-authored-by: Minh Chiến <131111976+MinhChien2302@users.noreply.github.com>
    Co-authored-by: MinhTan <21130171@st.hcmuaf.edu.vn>

--- resources/views/layout/web/header.blade.php
+++ resources/views/layout/web/header.blade.php
@@ -2,7 +2,6 @@
 <link href="{{asset('/lib/MDB_4_pro/css/mdb.lite.min.css')}}" rel="stylesheet">
 <link href="{{asset('/lib/MDB_4_pro/css/mdb.min.css')}}" rel="stylesheet">
 <link href="{{asset('/lib/MDB_4_pro/css/style.css')}}" rel="stylesheet">
-{{--<link href="{{asset('/lib/fontawesome-free-6.4.2-web/css/fontawesome.min.css')}}" rel="stylesheet">--}}
 <link href="{{asset('/lib/fontawesome-free-6.4.2-web/css/all.min.css')}}" rel="stylesheet">
 
 
--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Route::get('/', function () {
+//    return view('layout.web.main');
+//});
 Route::get('/', function () {
-    return view('layout.web.main');
+    return view('template.main');
 });
 
 Route::get('/intro', function (){
