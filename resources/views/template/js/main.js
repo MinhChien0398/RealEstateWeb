994d09fcc1762140d64631d649ac3db8b3b93811
MinhTan <21130171@st.hcmuaf.edu.vn>
2023-11-02 02:32:55 +0700

    fixed error for all page

--- resources/views/template/js/main.js
+++ resources/views/template/js/main.js
@@ -9,29 +9,6 @@ for (let i = 0; i < btns.length; i++) {
     });
 }
 
-let swiper = new Swiper(".slide-content", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-});
-
-
-let categorylist = document.getElementById("HomeProjectCategory");
-let categoryBtns = categorylist.getElementsByClassName("category-item");
-
-for (let i = 0; i < categoryBtns.length; i++) {
-    categoryBtns[i].addEventListener("click", function () {
-        for (let j = 0; j < categoryBtns.length; j++) {
-            if (categoryBtns[j].classList.contains("active")) {
-                categoryBtns[j].classList.remove(["active"]);
-            }
-        }
-        this.classList.add(["active"]);
-    });
-}
 
 function myFunction(x) {
     if (x.matches) {
