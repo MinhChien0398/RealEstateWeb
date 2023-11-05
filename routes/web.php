a520a383a5d249a130aed0b0ec48da5ac9ff33ea
MinhTan <21130171@st.hcmuaf.edu.vn>
2023-11-05 09:01:47 +0700

    update data branch

---
+++ routes/web.php
@@ -0,0 +1,84 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('template.main');
+});
+Route::get('/services', function () {
+    return view('template.services');
+});
+Route::get('/project', function () {
+    return view('template.project');
+});
+
+Route::get('/intro', function () {
+    return view('template.intro');
+});
+
+Route::get('/contact', function () {
+    return view('template.contact');
+});
+
+Route::get('/servicePost', function () {
+    return view('template.servicePost');
+});
+Route::get('/projectPost', function () {
+    return view('template.projectPost');
+});
+
+Route::get('/userManage', function () {
+    return view('template.UserManage');
+});
+Route::get('/roleManagement', function () {
+    return view('template.admin-RoleManagement');
+});
+Route::get('/slideManagement', function () {
+    return view('template.admin-slideManagement');
+});
+
+Route::get('/projectsManage', function () {
+    return view('template.projectsmanage');
+});
+Route::get('/typeOfProject', function () {
+    return view('template.admin-TypeOfProject');
+});
+Route::get('/postProject', function () {
+    return view('template.admin-PostProject');
+});
+Route::get('/contactManagement', function () {
+    return view('template.admin-contactManagement');
+});
+Route::get('/contactTypeManagement', function () {
+    return view('template.admin-ContactTypeManagement');
+});
+
+Route::get('/servicesManager', function () {
+    return view('template.servicesManager');
+});
+Route::get('/user', function () {
+    return view('template.user');
+});
+Route::get('/userFavourite', function () {
+    return view('template.user_favourite');
+});
+Route::get('/userHistory', function () {
+    return view('template.user_history');
+});
+Route::get('/login', function () {
+    return view('template.login');
+});
+Route::get('/dashboard', function () {
+    return view('template.dashboard');
+});
