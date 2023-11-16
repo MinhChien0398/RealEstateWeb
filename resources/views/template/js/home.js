f937d7812bd95cc0975ccb16dcac01e032026173
Minh Chien <21130010@st.hcmuaf.edu.vn>
2023-11-16 11:25:35 +0700

    update link img, page

--- resources/views/template/js/home.js
+++ resources/views/template/js/home.js
@@ -7,18 +7,25 @@ let swiper = new Swiper(".slide-content", {
     },
 });
 
-let items= document.querySelectorAll('.item-selector');
+let items = document.querySelectorAll('.item-selector');
 
-let cur=document.getElementsByClassName('item-selector active')
+let cur = document.getElementsByClassName('item-selector active')
 for (const item of items) {
-    item.addEventListener('click',function () {
-      for(const i of items){
-          if(i!=item){
-              i.classList.remove('active')
-            document.getElementById(i.getAttribute('type')).classList.remove('active')
-          }
-      }
-      item.classList.add('active')
+    item.addEventListener('click', function () {
+        for (const i of items) {
+            if (i != item) {
+                i.classList.remove('active')
+                document.getElementById(i.getAttribute('type')).classList.remove('active')
+            }
+        }
+        item.classList.add('active')
         document.getElementById(item.getAttribute('type')).classList.add('active')
     })
 }
+
+// while click fa-regular fa-bookmark change to fa-solid fa-bookmark
+$(document).ready(function () {
+    $('.fa-bookmark').click(function () {
+        $(this).toggleClass('fa-regular fa-bookmark fa-solid fa-bookmark');
+    });
+});
