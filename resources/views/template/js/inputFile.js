fd5a9d1298dd421acb111e3e2f3cf7223db9dd4d
MinhTan <21130171@st.hcmuaf.edu.vn>
2023-11-16 08:50:16 +0700

    update admin page after config

--- resources/views/template/js/inputFile.js
+++ resources/views/template/js/inputFile.js
@@ -1,13 +1,13 @@
 let fileInput = document.getElementById("inputGroupFile01");
 let fileInput1 = document.getElementById("inputGroupFile0");
 let imageContainer = document.getElementById("images");
-let imageContainer1 = document.getElementById("images1");
-let numOfFiles = document.getElementById("num-of-files");
+let imageContainer1 = document.getElementById("GroupImage");
+let numOfFiles = document.getElementById("num-of-files")    ;
 let numOfFiles1 = document.getElementById("num-of-files1");
 
 function preview(){
     imageContainer.innerHTML = "";
-    numOfFiles.textContent = `${fileInput.files.length} Files Selected`;
+    numOfFiles.textContent = `Đã chọn ${fileInput.files.length} `;
 
     for(i of fileInput.files){
         let reader = new FileReader();
@@ -18,6 +18,7 @@ function preview(){
         reader.onload=()=>{
             let img = document.createElement("img");
             img.setAttribute("src",reader.result);
+            console.log(URL.createObjectURL(reader.result));
             figure.insertBefore(img,figCap);
         }
         imageContainer.appendChild(figure);
@@ -26,17 +27,24 @@ function preview(){
 }
 function preview1(){
     imageContainer1.innerHTML = "";
-    numOfFiles1.textContent = `${fileInput1.files.length} Files Selected`;
+    numOfFiles1.textContent = `Đã chọn ${fileInput1.files.length}`;
 
     for(i of fileInput1.files){
         let reader = new FileReader();
         let figure = document.createElement("figure");
         let figCap = document.createElement("figcaption");
+        figure.classList.add("col-lg-4");
+        figure.classList.add("col-md-6");
+        figure.classList.add("col-sm-12");
+        figure.classList.add("mb-4");
         figCap.innerText = i.name;
+        figCap.classList.add('text-center')
         figure.appendChild(figCap);
         reader.onload=()=>{
             let img = document.createElement("img");
             img.setAttribute("src",reader.result);
+            img.classList.add("w-100");
+            img.classList.add("h-auto");
             figure.insertBefore(img,figCap);
         }
         imageContainer1.appendChild(figure);
