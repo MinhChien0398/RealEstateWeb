6b8701615c35bdc14ea34bb2b157f3d6908bd514
MinhTan <21130171@st.hcmuaf.edu.vn>
2023-11-16 23:34:42 +0700

    update form and post

--- resources/views/template/js/user.js
+++ resources/views/template/js/user.js
@@ -1,14 +1,3 @@
-$('document').ready(function () {
-    $(".button-collapse").sideNav();
-    var sideNavScrollbar = document.querySelector('.custom-scrollbar');
-    var ps = new PerfectScrollbar(sideNavScrollbar);
-});
-
-$('.datepicker').pickadate({
-    weekdaysShort: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
-    showMonthsShort: true
-});
-
 $(document).ready(function () {
     $('.mdb-select').materialSelect();
 });
