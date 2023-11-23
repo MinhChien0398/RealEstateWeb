3e273dfd796b34765ed2babba8f1d17b7d7fbf89
MinhTan <21130171@st.hcmuaf.edu.vn>
2023-11-23 23:45:30 +0700

    start to seperate

--- resources/views/layout/web/header.blade.php
+++ resources/views/layout/web/header.blade.php
@@ -3,5 +3,6 @@
 <link href="{{asset('/lib/MDB_4_pro/css/mdb.min.css')}}" rel="stylesheet">
 <link href="{{asset('/lib/MDB_4_pro/css/style.css')}}" rel="stylesheet">
 <link href="{{asset('/lib/fontawesome-free-6.4.2-web/css/all.min.css')}}" rel="stylesheet">
+<link href="{{asset('/lib/fontawesome-free-6.4.2-web/css/all.min.css')}}" rel="stylesheet">
 
 
